b631861b56a90820576f60ed4014e108dfe647b8
sohanr <mizan.rahman66d@gmail.com>
2022-11-01 04:32:27 +0600

    validity check

--- main.js
+++ main.js
@@ -38,5 +38,23 @@ class Blockchain{
          
     }    
 
+    // checking block validity
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+            
+        }
+
+        return true;
+    }
 }
 

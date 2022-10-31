553177b46bb64593bab2f8c70e17210cedb26af9
sohanr <mizan.rahman66d@gmail.com>
2022-11-01 04:30:19 +0600

    creating block

---
+++ main.js
@@ -0,0 +1,14 @@
+const SHA256 = require('crypto-js/sha256');
+
+//creating block
+class Block{
+    constructor(index, timestamp, data, previousHash = ' '){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+  
+}
+
